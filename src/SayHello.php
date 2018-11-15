0df6f39e59f7f5e516b733bbec29745a5fbc5580
isjx <isjx@foxmail.com>
2018-11-15 11:28:09 +0800

    init

---
+++ src/SayHello.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * create by sublime
+ * author: j.x.
+ * date: 2018-11-15 12:08:30
+ */
+namespace try_package;
+
+class SayHello
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+	public static function helloWorld(){
+		echo 'hello world';
+	}
+}
